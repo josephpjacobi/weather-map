bd2ab80a43712d242ae4eb578a4f6a536ceef5b5
Joe Jacobi <joseph.p.jacobi@gmail.com>
2020-11-18 20:13:42 -0500

    displayed dummy data on the results page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import  LandingPage  from './components/landing-page/landing-page';
 import  ResultsPage  from "./components/results-page/results-page";
+import './App.css'
 
 function App() {
   const [searchResults, setSearchResults] = useState({present: false})
 
+  const getWeather = (weatherObject) => {
+    setSearchResults(
+      {
+        present: true,
+        results: weatherObject
+      })
+  }
+
   return (
     <div className="App">
-      {searchResults.present ? <ResultsPage /> : <LandingPage /> }
+      {searchResults.present ? <ResultsPage results={searchResults.results} /> : <LandingPage getWeather={getWeather} /> }
     </div>
   );
 }
